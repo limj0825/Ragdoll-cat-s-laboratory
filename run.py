c031613039a674da6a55a712ec769b41651131bb
limiaojin <931108604@qq.com>
2019-09-13 19:06:35 +0800

    add 404 page

--- run.py
+++ run.py
@@ -47,6 +47,11 @@ def judge():
             pass
 
 
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html')
+
+
 @app.route('/')
 def index():
     return render_template('index.html')
